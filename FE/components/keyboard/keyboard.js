2eb4f5672ef39f4475a568fad9c5408f822bea23
Daniele La Marca <71449152+xLordPvP@users.noreply.github.com>
2025-05-19 21:27:11 +0200

    fix-enter

--- FE/components/keyboard/keyboard.js
+++ FE/components/keyboard/keyboard.js
@@ -1,35 +1,45 @@
 document.addEventListener('keydown', function(e) {
     if(e.key === "Enter"){
         e.preventDefault();
-        sendMessage();
+        const active = document.activeElement;
+        if(active.id === 'keyboard-message'){
+            sendMessage();
+        }
     }
 });
 
 async function sendMessage(){
+    try{
     const isLogged = await isLoggedIn();
     if(!isLogged.isLogged){
-        console.log(isLogged);
         loadLogin();
     }
-    let message = document.getElementById("keyboard-message");
-    let chat_id = document.getElementById("main").getAttribute('chat_id');
     
-    if(message.value != "") {
-        
-        const res = await fetch(`${API_BASE_URL}/sendMessage`, {
-            method: 'POST',
-            credentials: 'include',
-            headers: {
-                'Content-type':'application/json'
-            },
-            body: JSON.stringify({
-                message: message.value,
-                chat_id: chat_id
-            })
+    let message = document.getElementById("keyboard-message");
+    if(message != ""){
+        let chat_id = document.getElementById("main").getAttribute('chat_id');
+    
+        if(message.value != "") {
+            
+            const res = await fetch(`${API_BASE_URL}/sendMessage`, {
+                method: 'POST',
+                credentials: 'include',
+                headers: {
+                    'Content-type':'application/json'
+                },
+                body: JSON.stringify({
+                    message: message.value,
+                    chat_id: chat_id
+                })
 
-        });
-        message.value = "";
-        loadLastMeesage(chat_id);
+            });
+            message.value = "";
+            loadLastMeesage(chat_id);
+        }
+    
+    }
+    }catch(e){
+        
     }
     
     // openChat(chat_id);
--- FE/pages/login/login.js
+++ FE/pages/login/login.js
@@ -1,15 +1,37 @@
+document.addEventListener('keydown', function(e) {
+    if(e.key === "Enter"){
+        e.preventDefault();
+        const active = document.activeElement;
+        let app = document.getElementById('app')
+
+        let loginelement = document.getElementById('login');
+        let registerelement = document.getElementById('register');
+
+        if(app.contains(loginelement))
+        {
+            login()
+        }else if(app.contains(registerelement)){
+            register()
+        }
+
+        
+    }
+});
+
 async function loadLogin() {
     try {
         const res = await fetch('./pages/login/login.html');
         const html = await res.text();
         document.getElementById("app").innerHTML = html;
 
-        // Adesso puoi aggiungere il listener
+        
         const registerLink = document.getElementById("register-link");
         if (registerLink) {
-            registerLink.addEventListener('click', function(){
+            registerLink.addEventListener('click', function(e){
+                console.log('register');
+                e.stopPropagation();
                 loadRegister();
-            });
+            },true);
         }
     } catch (err) {
         console.error("Errore nel caricamento della Home:", err);
